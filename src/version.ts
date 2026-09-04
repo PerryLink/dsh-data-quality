650f52bf1a9aeb85eceb4a4c25926f48ec7bfaec
PerryLink <novelnexusai@outlook.com>
2026-09-04 22:48:20 +0800

    chore(release): 0.3.6

--- src/version.ts
+++ src/version.ts
@@ -5,7 +5,7 @@
  */
 
 /** The package version reported in persisted reports. */
-export const VERSION = '0.3.5'
+export const VERSION = '0.3.6'
 
 /**
  * Version of the persisted report schema. Bump it whenever a report's
